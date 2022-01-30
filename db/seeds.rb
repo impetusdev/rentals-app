bd2f73a4a76923f3c8aa4e2da11fca828e90d067
Laurence Qi <laurence.qi@outlook.com>
2022-01-30 15:01:28 +1100

    finished Rental CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-#TODO: seed with rental and user data, see if the many to many associations is working. 
 User.destroy_all
 
 puts 'Creating new User seed data'
