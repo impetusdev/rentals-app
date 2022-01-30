04b5743c8e3b987cf5972ea01c47afca8696b391
Laurence Qi <laurence.qi@outlook.com>
2022-01-30 18:12:15 +1100

    Added Crud to destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,22 +1,37 @@
 class DestinationsController < ApplicationController
   def new
+    @destination = Destination.new
   end
 
   def create
+    Destination.create! destination_params
   end
 
   def index
+    @destinations = Destination.all
   end
 
   def show
+    @destination = Destination.find params[:id]
   end
 
   def edit
+    @destination = Destination.find params[:id]
   end
 
   def update
+    Destination.find(params[:id]).update! destination_params
+    redirect_to destination_path(params[:id])
+    # I think it's actually using the destination variable, and saving
   end
 
   def destroy
+    Destination.find(params[:id]).destroy
+    redirect_to destinations_path
   end
+
+  private def destination_param
+    params.require(:destination).permit Destination.list_params
+  end
+  
 end
