4be0bb89d7e7d814d3d912566019ce95436adc38
Laurence Qi <laurence.qi@outlook.com>
2022-01-30 15:01:28 +1100

    finished Rental CRUD

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,7 +2,7 @@ class Rental < ApplicationRecord
     has_and_belongs_to_many :users
     
     def self.list_params
-        [:street_address, :suburb, :price, :address]
+        [:street_address, :suburb, :price, :image]
     end
     
 end
