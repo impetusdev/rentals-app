0fae29840203e54546620a94703d00aada1de05b
Laurence Qi <laurence.qi@outlook.com>
2022-02-01 23:20:44 +1100

    added session controller skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'session/new' => 'session#new '
+  post 'session/create' => 'session#create'
+  delete 'session/destroy' => 'session#destroy'
+  
   resources :destinations
   resources :rentals
   # resources :users
