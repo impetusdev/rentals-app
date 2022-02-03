1f1e3c44a1a58f55718c57f1307f297142381d0e
Laurence Qi <laurence.qi@outlook.com>
2022-02-04 08:56:49 +1100

    cleaned up some code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,10 @@ class UsersController < ApplicationController
       username: params[:username],
       password: params[:password]
     )
-    
 
+    #TODO: form validation. 
     flash[:alert] = 'Account successfully created'
     redirect_to login_path
 
   end
-
-    private def user_params
-        params.require(:user).permit(:username, :password) 
-    end
   end
