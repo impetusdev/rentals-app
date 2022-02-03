c84bc4566b1336e4f5720f7edc448ec6549dca13
Laurence Qi <laurence.qi@outlook.com>
2022-02-04 08:27:33 +1100

    added a flash message to declare account made

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,9 @@ class UsersController < ApplicationController
       password: params[:password]
     )
     
-    
-    redirect_to rentals_path
 
-    # user = User.find_by username: params[:username]
-    #TODO: add form validation
-    # if user.present? && user.authenticate(params[:password]) # authenticate is a bcrypt method for checking password
-    #   session[:user_id] = user.id
-
-    #   redirect_to rentals_path
-    # else
-    #   # throw error and redirect to the login page
-    #   flash[:error] = 'Invalid email or password'
-    #   redirect_to rentals_path
-    # end
+    flash[:alert] = 'Account successfully created'
+    redirect_to login_path
 
   end
 
