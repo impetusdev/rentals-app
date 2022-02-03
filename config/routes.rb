f695481860882a7ed4d9803825e301652c7af03a
Laurence Qi <laurence.qi@outlook.com>
2022-02-04 07:47:42 +1100

    got popup working, form submission needs to be done properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root to: 'rentals#index'
+  
   get '/login' => 'session#new' # shows the login form. 
   post '/login' => 'session#create' #form submits to here
   delete '/login' => 'session#destroy' # logout
-  
+
+  post '/signup' => 'users#create' 
+
   resources :destinations
   resources :rentals
+  resources :users
   # resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
